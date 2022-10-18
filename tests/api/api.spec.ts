ebf2e2033ac2cf5bebea9f437a1a1c6a74002fa5
rakibul-shahar <78203754+Shaharislam@users.noreply.github.com>
2022-10-18 19:38:41 +0600

    Get-Request

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -11,4 +11,15 @@ test.describe.parallel('API Testing',()=>{
         const response = await request.get(`${(baseUrl)}/users/non-existing-endpoint`)
         expect(response.status()).toBe(404)
      })
+     test('Get Request - Get User Details',async({request})=>{
+        const response = await request.get(`${(baseUrl)}/users/1`)
+        const responseBody = JSON.parse(await response.text())
+
+        expect(response.status()).toBe(200)
+        expect(responseBody.data.id).toBe(1)
+        expect(responseBody.data.first_name).toBe('George')
+        expect(responseBody.data.last_name).toBe('Bluth')
+        expect(responseBody.data.email).toBeTruthy()
+    
+    })
 })
