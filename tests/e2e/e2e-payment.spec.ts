4b86a9f2b9abda47857f291cf0fcefae3c682f59
rakibul-shahar <78203754+Shaharislam@users.noreply.github.com>
2022-10-18 13:02:11 +0600

    NavBar Page Add with POM

--- tests/e2e/e2e-payment.spec.ts
+++ tests/e2e/e2e-payment.spec.ts
@@ -1,14 +1,20 @@
 import{ test, expect } from '@playwright/test'
 import{LoginPage} from '../../page-objects/LoginPage'
 import{HomePage} from '../../page-objects/HomePage'
+import{PaymentPage} from '../../page-objects/PaymentPage'
+import{NavBar} from '../../page-objects/components/NavBar'
 
-test.describe('New Payment',() => {
+test.describe.only('New Payment',() => {
    let loginPage:LoginPage
    let  homePage:HomePage
+   let  payMentPage:PaymentPage
+   let  navBar:NavBar
 
     test.beforeEach(async ({page})=>{
       loginPage=new LoginPage(page)
       homePage =new HomePage(page)
+      navBar =new NavBar(page)
+
       await homePage.visit()
       await homePage.clickOnSignIn()
       await loginPage.login('username','password')
@@ -18,21 +24,10 @@ test.describe('New Payment',() => {
     })
     test( 'Should send new payment', async ({page})  => {
        //Here goes the test code
-       await page.click('#pay_bills_tab')
-       await page.selectOption('#sp_payee','apple')
-       await page.click('#sp_get_payee_details')
-       await page.waitForSelector('#sp_payee_details')
-       await page.selectOption('#sp_account','6')
-       await page.type('#sp_amount','5000')
-       await page.type('#sp_date','2022-18-10')
-       await page.type('#sp_description','Some Random Message')
-       await page.click('#pay_saved_payees')
-
-       const message= await page.locator('#alert_content > span')
-       await expect(message).toBeVisible()
-       await expect(message).toContainText('The payment was successfully submitted')
+       await navBar.clickOnTab('Pay Bills')
+       await payMentPage.createPayment()
+       await payMentPage.assertSuccessMessage()
     
     })
-    
 
  })
